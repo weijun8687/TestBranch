4e2e7d36cf44ac6579110ece1f0a8a13fc6ab2f9
weijun8687 <weiguojun@zhinantech.com>
2019-08-29 21:28:27 +0800

    new two file

---
+++ TestBranch/two/TwoViewController.m
@@ -0,0 +1,32 @@
+//
+//  TwoViewController.m
+//  TestBranch
+//
+//  Created by WJ on 2019/8/29.
+//  Copyright © 2019年 WJ. All rights reserved.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
